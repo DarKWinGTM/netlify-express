1c39e4b4e9af6bbac76a38476b5cd6faa9bf22d7
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-10-13 22:06:02 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -2,13 +2,25 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
 
+const crypto                                = require('crypto');
+const { Api, JsonRpc, Serialize }           = require('eosjs');
+const { JsSignatureProvider, PrivateKey }   = require('eosjs/dist/eosjs-jssig');
+const fetch                                 = require('node-fetch');
+const { TextEncoder, TextDecoder }          = require('text-encoding');
+const url                                   = require('url');
+const fs                                    = require('fs'); 
+
+const app = express();
 const router = express.Router();
+
+const privateKeys = ['5KJEamqm4QT2bmDwQEmRAB3EzCrCmoBoX7f6MRdrhGjGgHhzUyf']; 
+const signatureProvider = new JsSignatureProvider(privateKeys);
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Express.js!</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
@@ -16,7 +28,22 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html'))); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
