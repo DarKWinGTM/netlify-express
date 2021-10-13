fb50d2bb63d8f693319fa6ae15bea1adc0e9f318
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-10-13 23:18:04 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -29,16 +29,17 @@ router.get("/", (req, res) => {
     
 });
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+//  router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+//  router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/echo', (req, res) => {
+// app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+router.get('/echo', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.end(`ECHO : ${req.url }`);
 });
 
-module.exports = app;
+// Serverless
+// app.use('/.netlify/functions/index', router);
 module.exports.handler = serverless(app);
