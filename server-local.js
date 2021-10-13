241bf01e21a5c3f9cc0b110fdba521ba21c1acf1
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-10-14 02:03:35 +0700

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,26 +1,3 @@
-//        'use strict';
-
-//				const app       = require('./express/server');
-//				const cpus      = require('os').cpus();
-//				const cluster   = require('cluster');
-//				
-//				const nodeType  = (cluster.isMaster) ? 'Master' : 'Worker';
-//				
-//				if (cluster.isMaster) {
-//				    for (let i = 0; i < 4; i++) {
-//				        cluster.fork();
-//				    }; 
-//				    cluster.on('exit', (worker, code, signal) => {
-//				        console.log('Worker #' + worker.process.pid, 'exited');
-//				        cluster.fork();
-//				    }); 
-//				} else {
-//				    app.listen(3000, () => console.log('Local app listening on port 3000!'));
-//				}; 
-//				
-//				console.log(nodeType + ' #' + process.pid, 'is running');
-
-
 'use strict';
 const express     = require('express');
 const path        = require('path');
