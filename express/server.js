a8dec969535f68a692a41e19dfc76aec8b38449a
DarKWinGTM <tonmaster.1.08@hotmail.com>
2021-10-13 23:05:17 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,24 +6,14 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/echo', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`ECHO : ${req.url }`);
-});
-
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express.js!</h1>');
+// //   res.end();
+// });
 // Home route
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     
     //  sets the header of the response to the user and the type of response that you would be sending back
     res.setHeader('Content-Type', 'text/html');
@@ -38,5 +28,17 @@ app.get("/", (req, res) => {
     res.end(); 
     
 });
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/echo', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`ECHO : ${req.url }`);
+});
+
 module.exports = app;
 module.exports.handler = serverless(app);
